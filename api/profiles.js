e1ed5915d2d1d66e9d7e1f019e4b492d3a11677b
azinzua-debug <azinzua@gmail.com>
2025-11-10 20:39:36 -0300

    Delete api/profiles.js

--- api/profiles.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const data = [
-    { nombre: "fuegito", descripcion: "hola", redes: "ig", rol: "ambos" }
-  ];
-  res.status(200).json(data);
-}
